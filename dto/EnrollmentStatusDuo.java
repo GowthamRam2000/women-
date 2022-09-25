8c130a0cecdc829b6eeaa40b10635d957bed06e6
NotHenryCavill <gowthamm161@gmail.com>
2022-09-25 20:38:35 +0530

    Create EnrollmentStatusDuo.java

---
+++ dto/EnrollmentStatusDuo.java
@@ -0,0 +1,48 @@
+package com.lti.dto;
+
+public class EnrollmentStatusDto extends StatusDto {
+	int userId;
+	int ngoId;
+	int courseId;
+	String ngoName;
+	String courseName;
+	String employmentStatus;
+	
+	public String getEmploymentStatus() {
+		return employmentStatus;
+	}
+	public void setEmploymentStatus(String employmentStatus) {
+		this.employmentStatus = employmentStatus;
+	}
+	public int getCourseId() {
+		return courseId;
+	}
+	public void setCourseId(int courseId) {
+		this.courseId = courseId;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public int getNgoId() {
+		return ngoId;
+	}
+	public void setNgoId(int ngoId) {
+		this.ngoId = ngoId;
+	}
+	public String getNgoName() {
+		return ngoName;
+	}
+	public void setNgoName(String ngoName) {
+		this.ngoName = ngoName;
+	}
+	public String getCourseName() {
+		return courseName;
+	}
+	public void setCourseName(String courseName) {
+		this.courseName = courseName;
+	}
+	
+}
