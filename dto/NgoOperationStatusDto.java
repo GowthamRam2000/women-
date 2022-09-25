f52a596b4846cae073b78cf930601662e8ae199f
NotHenryCavill <gowthamm161@gmail.com>
2022-09-25 20:41:09 +0530

    Create NgoOperationStatusDto.java

---
+++ dto/NgoOperationStatusDto.java
@@ -0,0 +1,15 @@
+
+package com.lti.dto;
+
+public class NgoOperationStatusDto extends StatusDto {
+	String ngoName;
+
+	public String getNgoName() {
+		return ngoName;
+	}
+
+	public void setNgoName(String ngoName) {
+		this.ngoName = ngoName;
+	}
+	
+}
